a25c401ef9bb7a58027a5fddd6262622ee87984c
Guilherme Caeiro de Mattos <guilhermecaeirodemattos@gmail.com>
2018-05-07 11:47:13 -0300

    Fixed the problem where the WiSARD would use only the last sample to train a discriminator and added support to bleaching.

--- Wisard.py
+++ Wisard.py
@@ -4,18 +4,20 @@ import numpy as np
 import copy
 
 class Discriminator:
-    def __init__(self, input_class, input_length, tupple_size):
+    def __init__(self, input_class, input_length, tupple_size, bleaching = False):
         self.input_class = input_class
         self.input_length = input_length
         self.tupple_size = tupple_size
         self.ram_size = math.pow(self.tupple_size, 2)
         self.number_of_rams = int(self.input_length / self.tupple_size)
+        self.bleaching = bleaching
         #self.memory = np.zeros(self.number_of_rams, self.ram_size)
         self.memory = {}
         
     def write(self, pattern):
         for i in range(self.number_of_rams):
-            self.memory[i] = {}
+            if i not in self.memory:
+                self.memory[i] = {}
             address = []
 
             for j in range(i * self.tupple_size, (i * self.tupple_size) + self.tupple_size):
@@ -24,14 +26,16 @@ class Discriminator:
             address = "".join(str(i) for i in address) # address becomes a string
 
             if address not in self.memory[i]:
+                #print("Memory Created")
                 self.memory[i][address] = 1
-            else:
-                #self.memory[i][address] = self.memory[i][address] + 1
-                pass
+            elif address in self.memory[i] and self.bleaching:
+                #print("Memory Incremented")
+                self.memory[i][address] = self.memory[i][address] + 1
+                #pass
 
         #print("Writting pattern in memory.", self.memory)
             
-    def evaluate(self, pattern):
+    def evaluate(self, pattern, bleaching_threshold = 0):
         score = 0
 
         for i in range(self.number_of_rams):
@@ -43,16 +47,24 @@ class Discriminator:
             address = "".join(str(i) for i in address) # address becomes a string
 
             if i in self.memory and address in self.memory[i]:
-                score = score + 1
+                if self.bleaching and self.memory[i][address] >= bleaching_threshold:
+                    print("Threshold: ", bleaching_threshold, "Somado: ", 1)
+                    score = score + 1
+                elif self.bleaching and self.memory[i][address] < bleaching_threshold:
+                    print("Threshold: ", bleaching_threshold, "Somado: ", 0)
+                    continue
+                else:
+                    score = score + 1
                 
-        return score
+        return score 
 
 class Wisard:
 
-    def __init__(self, tupple_size = 2, seed = 0):
+    def __init__(self, tupple_size = 2, seed = 0, bleaching = False):
         #self.input_list = []
         #self.expected_output_list = []
         self.seed = seed
+        self.bleaching = bleaching
         self.tupple_size = tupple_size
         self.discriminators = []
 
@@ -75,7 +87,7 @@ class Wisard:
 
             return input_classes
         elif mode == "prediction":
-        	#input_item = input_list[0]
+            #input_item = input_list[0]
             input_item = copy.deepcopy(input_list[0])
             #print("Original pattern: ", input_item)
             random.seed(self.seed)
@@ -97,7 +109,7 @@ class Wisard:
             print("Number of training samples for class " + str(input_class) + ": " + str(len(input_classes[input_class])))
 
             input_data_length = len(input_classes[input_class][0])
-            discriminator = Discriminator(input_class, input_data_length, self.tupple_size)
+            discriminator = Discriminator(input_class, input_data_length, self.tupple_size, self.bleaching)
 
             for training_sample in input_classes[input_class]:
                 discriminator.write(training_sample)
@@ -107,16 +119,58 @@ class Wisard:
     def predict(self, rawinput):
         processed_input = self.processInput("prediction", [rawinput])
 
-        predicted_class = {"class": "", "score": 0}
+        result_achieved = False
 
-        for discriminator in self.discriminators:
-            #print("Evaluating with discriminator for class \"" + discriminator.input_class + "\".")
-            score = discriminator.evaluate(processed_input)
-            if score > predicted_class["score"]:
-                predicted_class["class"] = discriminator.input_class
-                predicted_class["score"] = score
+        predicted_classes = []
+        current_threshold = 0
 
-        return predicted_class
+        discriminators_to_evaluate = self.discriminators
+
+        while not result_achieved:
+            predicted_classes = [{"discriminator": None, "score": 0}]
+            #predicted_class = {"class": "", "score": 0}
+
+            for discriminator in discriminators_to_evaluate:
+                print("Evaluating with discriminator for class \"" + discriminator.input_class + "\".")
+                score = discriminator.evaluate(processed_input, current_threshold)
+                print("Score for discriminator", discriminator.input_class, ": ", score)
+                if score > predicted_classes[0]["score"]:
+                    predicted_classes = [{"discriminator": discriminator, "score": score}]
+                elif score == predicted_classes[0]["score"]:
+                    predicted_classes.append({"discriminator": discriminator, "score": score})
+
+
+            if not self.bleaching:
+                print("Nada de Bleaching, sefinir!")
+                result_achieved = True
+            elif self.bleaching and len(predicted_classes) > 1:
+                print("Yahho!", len(predicted_classes))
+                if predicted_classes[0]["score"] == 1:
+                    print("Score 1")
+                    result_achieved = True
+
+
+                current_threshold = current_threshold + 1
+
+                discriminators_to_evaluate = []
+                for predicted_class in predicted_classes:
+                    discriminators_to_evaluate.append(predicted_class["discriminator"])
+
+            elif self.bleaching and len(predicted_classes) == 1:
+                print("Pimba!")
+                result_achieved = True
+            else:
+                print("Error predicting class.")
+                break
+
+        # If the method ends with more than one class as possible, it just returns the first one
+        return {"class": predicted_classes[0]["discriminator"].input_class, "score": predicted_classes[0]["score"]}
+
+    def deactivate_bleaching(self):
+        self.bleaching = False
+
+    def show_mental_map(self):
+        pass
 
     def save_network_to_disk(self):
         pass
--- test.py
+++ test.py
@@ -16,13 +16,21 @@ test_set = [
                     [[1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1], "H"],
                     [[1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1], "A"],
                     [[1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1], "O"],
+                    [[1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1], "H"],
+                    [[1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1], "A"],
                 ]
 
 training_patterns = [row[0] for row in training_set]
 expected_outputs = [row[1] for row in training_set]
 
-wann = Wisard(2, 3546)
+wann = Wisard(2, 3546, False)
 wann.train(training_patterns, expected_outputs)
+
+for discriminator in wann.discriminators:
+    print(discriminator.input_class)
+    print(discriminator.input_length)
+    print(discriminator.tupple_size)
+    print(discriminator.memory)
 
 
 print("\n\n\n")
@@ -31,7 +39,7 @@ for pattern in test_set:
     print("Evaluating pattern ", pattern)
     print("Result: ", wann.predict(pattern[0]), "Expected: ", pattern[1], "\n")
 
-for pattern in test_set:
+"""for pattern in test_set:
     print("Evaluating pattern ", pattern)
     print("Result: ", wann.predict(pattern[0]), "Expected: ", pattern[1], "\n")
 
@@ -69,3 +77,40 @@ for pattern in test_set:
     print("Result: ", wann.predict(pattern[0]), "Expected: ", pattern[1], "\n")
 
 
+"""
+
+wann = Wisard(2, 3546, True)
+wann.train(training_patterns, expected_outputs)
+
+for discriminator in wann.discriminators:
+    print(discriminator.input_class)
+    print(discriminator.input_length)
+    print(discriminator.tupple_size)
+    print(discriminator.memory)
+
+
+print("\n\n\n")
+
+for pattern in test_set:
+    print("Evaluating pattern ", pattern)
+    print("Result: ", wann.predict(pattern[0]), "Expected: ", pattern[1], "\n")
+
+
+
+wann = Wisard(2, 3546, True)
+wann.train(training_patterns, expected_outputs)
+
+for discriminator in wann.discriminators:
+    print(discriminator.input_class)
+    print(discriminator.input_length)
+    print(discriminator.tupple_size)
+    print(discriminator.memory)
+
+
+wann.deactivate_bleaching()
+
+print("\n\n\n")
+
+for pattern in test_set:
+    print("Evaluating pattern ", pattern)
+    print("Result: ", wann.predict(pattern[0]), "Expected: ", pattern[1], "\n")
